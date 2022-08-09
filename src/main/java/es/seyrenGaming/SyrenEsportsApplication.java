d813948a7936e03b58a407a0470b2cf00052df10
raulrodriguezlr <12rulo12@gmail.com>
2022-08-09 18:41:07 +0200

    creado proyecto

---
+++ src/main/java/es/seyrenGaming/SyrenEsportsApplication.java
@@ -0,0 +1,13 @@
+package es.seyrenGaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyrenEsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyrenEsportsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/es/seyrenGaming/WebController.java
@@ -0,0 +1,28 @@
+package es.seyrenGaming;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	@GetMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
+	@GetMapping("/error")
+	public String error() {
+		return "error";
+	}
+	
+}
